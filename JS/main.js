c809479f802e98c1b2888c2d6df3526e8aa22321
Dagart34 <magomedovi522@gmail.com>
2025-10-08 16:40:24 +0300

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,10 @@
+let score = 0;
+
+const scoreDisplay = document.getElementById('score');
+const clickButton = document.getElementById('clickButton');
+
+clickButton.addEventListener('click', () => {
+    //score++;
+    //scoreDisplay.textContent = score;
+    //onclick = window.location.href="UI-pages/register.html"
+});
